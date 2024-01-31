11cac4a0c35a447aac2baf160f7bf0e38498cd0c
Jonathan Lei <jincheng.lei@edu.sait.ca>
2024-01-31 06:36:34 +0000

    feat: week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Jincheng Lei</p>
+      <p>
+        <a href="https://github.com/jon-sait/cprg306-assignments">
+          https://github.com/jon-sait/cprg306-assignments
+        </a>
+      </p>
+    </div>
+  );
+}
